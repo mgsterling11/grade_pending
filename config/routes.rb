210b7a3311eb240e2780620f16ad66994535152b
mgsterling11 <m.g.sterling11@gmail.com>
2015-10-31 18:40:04 -0400

    cuisine type and restaurant name search working, moving on to multiple results display

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :restaurants
   get '/' => 'welcome#index' 
   get '/search' => 'search#index'
+  get '/search/multiple_results' => 'search#multiple_results'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
